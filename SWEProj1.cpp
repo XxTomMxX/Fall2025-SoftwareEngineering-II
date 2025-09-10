85e5140929868a5cb6eacf5bae001a20108beffa
Tom McLaughlin <159816452+XxTomMxX@users.noreply.github.com>
2025-09-10 17:39:02 -0500

    Add files via upload

---
+++ SWEProj1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string dog;
+    cout<<"is your dog dirty?: ";
+    cin>>dog;
+
+    if (dog == "yes"){
+        cout<<"your dog has mainge"<<endl;
+    }
+    else{
+    cout<<"dog is good";
+    }
+
+
+}
